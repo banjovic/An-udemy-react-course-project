f9215ae2b6655f995cdeed3e0bca168ef051e066
banjovic <banjovictoria25@gmail.com>
2022-01-24 19:07:31 +0100

    following instructions in the Udemy course

--- src/App.js
+++ src/App.js
@@ -33,32 +33,17 @@ function App() {
       title={expenses[1].title}
       amount={expenses[1].amount}
       date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
       title={expenses[2].title}
       amount={expenses[2].amount}
       date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
       title={expenses[3].title}
       amount={expenses[3].amount}
       date={expenses[3].date}
-      ></ExpenseItem>
-      <ExpenseItem
-      title={expenses[4].title}
-      amount={expenses[4].amount}
-      date={expenses[4].date}
-      ></ExpenseItem>
-      <ExpenseItem
-      title={expenses[5].title}
-      amount={expenses[5].amount}
-      date={expenses[5].date}
-      ></ExpenseItem>
-      <ExpenseItem
-      title={expenses[6].title}
-      amount={expenses[6].amount}
-      date={expenses[6].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
