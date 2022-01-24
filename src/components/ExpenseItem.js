fa630a2dc435bd9027663f0c8f438f2389b5fdb9
banjovic <banjovictoria25@gmail.com>
2022-01-24 11:58:13 +0100

    commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+function ExpenseItem (props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
+    
     return (
-        <div>
-            
+        <div className='expense-item'>
+            <div>
+                <div>{month}</div>
+                <div>Year</div>
+                <div>Day</div>
+            </div>
+            <div className='expense-item_description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item_price'>$ {props.amount}</div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ExpenseItem;
